4ffac01e0db28da629222e91ca1be50203f495ef
Patrick Sidharta <prdsidharta@gmail.com>
2026-06-16 22:34:56 -0400

    chore: Define project goals and objectives

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+
+/**
+ * NFL Draft Valuation and Re-Draft Engine
+ * 1. Parse historical NFL draft data, pre-draft evaluations, and team needs from specific years
+ * 2. Calculate expected value for every draftee and draft number
+ * 3. Calculate actual value of every draftee based off NFL career
+ * 4. Create a Re-Draft that simulates an optimal NFL draft based on actual value and historical team needs
+ * 
+ */
+
+int main(){
+
+    return 0;
+}
